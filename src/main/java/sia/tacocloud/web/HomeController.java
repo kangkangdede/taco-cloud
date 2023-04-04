53f60eb070cc4961f796c07b2f075d5d950b9676
kangkangdede <kangkangdede@163.com>
2023-04-04 23:14:20 +0800

    Add Taco Projects
    Add Taco Projects

---
+++ src/main/java/sia/tacocloud/web/HomeController.java
@@ -0,0 +1,14 @@
+package sia.tacocloud.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home(){
+        System.out.println("home page....");
+        System.out.println("home page 2....");
+        return "home";
+    }
+}
